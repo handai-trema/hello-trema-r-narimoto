0fba2cf33224ea143ed8de759e6445a834d01fc6
Ryosuke NARIMOTO <r-narimoto@ist.osaka-u.ac.jp>
2016-10-05 16:25:02 +0900

    mod start() and add disconnected

--- lib/hello_trema.rb
+++ lib/hello_trema.rb
@@ -1,10 +1,13 @@
 # Hello World!
 class HelloTrema < Trema::Controller
-  def start(_args)
-    logger.info 'Trema started.'
+  def start(args)
+    logger.info "#{args.class.name} started."
   end
 
   def switch_ready(datapath_id)
-    logger.info "Hello #{datapath_id.to_hex}!"
+    logger.info format('Hello %#x!', datapath_id)
+  end
+  def switch_disconnected(datapath_id)
+    logger.info format('Bye %#x', datapath_id)
   end
 end
